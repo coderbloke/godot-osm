254533f24a30332244ed4a1f8bf406ec97f3e02a
coderbloke <75695649+coderbloke@users.noreply.github.com>
2023-05-27 16:33:12 +0200

    Overpass test panel: Fix controlling builder and client

--- OverpassTestPanel.gd
+++ OverpassTestPanel.gd
@@ -7,7 +7,16 @@ class_name OverpassTestPanel extends VBoxContainer
 		if overpass_client != null:
 			overpass_client.base_url = base_url
 			
-@export var overpass_query_builder: OverpassQueryBuilder
+@export var compact: bool = false:
+	set(new_value):
+		compact = new_value
+		if overpass_client != null:
+			overpass_client.compact_query = compact
+@export var overpass_query_builder: OverpassQueryBuilder:
+	set(new_value):
+		overpass_query_builder = new_value
+		if overpass_client != null:
+			overpass_client.query_builder = overpass_query_builder
 @export var overpass_query: OverpassQuery
 
 @export var auto_query = true:
@@ -16,7 +25,11 @@ class_name OverpassTestPanel extends VBoxContainer
 		if auto_query_checkbutton != null:
 			auto_query_checkbutton.button_pressed = auto_query
 @export var print_to_console = true
-@export var trigger_query = false
+@export var trigger_query = false:
+	set(new_value):
+		if new_value: do_query()
+
+@export var osm_data: OSMData
 
 @onready var overpass_client: OverpassClient = $OverpassClient
 
@@ -28,7 +41,10 @@ class_name OverpassTestPanel extends VBoxContainer
 
 func _ready():
 	overpass_client.base_url = base_url
+	overpass_client.query_builder = overpass_query_builder
+	overpass_client.compact_query = compact
 	auto_query_checkbutton.button_pressed = auto_query
+	overpass_client.query_completed.connect(_on_query_completed)
 
 func _on_overpass_query_changed():
 	print("_on_overpass_query_changed()")
@@ -55,13 +71,13 @@ var _last_query_url: String
 
 func _update_ql_script():
 	if overpass_query_builder != null and overpass_query != null:
-		_ql_script = overpass_query_builder.get_overpass_ql_script(overpass_query)
+		_ql_script = overpass_query_builder.get_overpass_ql_script(overpass_query, compact)
 	else:
 		_ql_script = ""
 		
 func _update_query_url():
 	if overpass_query_builder != null and overpass_query != null:
-		_query_url = overpass_query_builder.get_overpass_ql_script(overpass_query, true)
+		_query_url = overpass_client.get_query_url(overpass_query)
 	else:
 		_query_url = ""
 
@@ -70,9 +86,8 @@ func do_query():
 		_last_query_url = _query_url
 		overpass_client.cancel_request()
 		if print_to_console:
-			print_rich("[color=Lightskyblue][OverpassTestPanel.do_query] TODO[/color]")
-			#print_rich("[color=Lightskyblue]" + overpass_client.get_query_url(overpass_query) + "[/color]")
-		#overpass_client.query(overpass_query)
+			print_rich("[color=Lightskyblue]" + overpass_client.get_query_url(overpass_query) + "[/color]")
+		overpass_client.query(overpass_query)
 
 func _process(delta):
 	query_resource_value_label.text = get_resource_info(overpass_query)
@@ -87,8 +102,12 @@ func _process(delta):
 func _on_query_button_pressed():
 	do_query()
 
-func _on_query_completed(result: Array):
-	print("[OverpassTestPanel._on_query_completed]")
+func _on_query_completed(result: Dictionary):
+	osm_data = OSMData.new()
+	overpass_client.update_osm_data_from_query_result(osm_data, result)
+	if print_to_console:
+		print("Retrieved %d nodes, %d way and %d relations." \
+			% [osm_data.nodes.size(), osm_data.ways.size(), osm_data.relations.size()])
 
 func _on_auto_query_check_button_toggled(button_pressed):
 	auto_query = button_pressed
